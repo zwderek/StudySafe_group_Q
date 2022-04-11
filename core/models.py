62f33805de851a9065fbdb120b4190784de097ea
unknown <zwd1025@connect.hku.hk>
2022-04-11 21:34:47 +0800

    update model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class HKU_member(models.Model):
+    hku_id = models.CharField(max_length=10, primary_key=True)
+    name = models.CharField(max_length=150)
+    def __str__(self):
+        return f'{self.hku_id} {self.name}'
+
+class Venue(models.Model):
+    VENUE_TYPES = (
+        ('LT', 'Lecture Theatre'),
+        ('CR', 'Classroom'),
+        ('TR', 'Tutorial Room'),
+    )
+    venue_code = models.CharField(max_length=20, primary_key=True)
+    location = models.CharField(max_length=150)
+    type = models.CharField(max_length=2, choices=VENUE_TYPES)
+    capacity = models.IntegerField()
+    def __str__(self):
+        return self.venue_code
+
+class EntryExit(models.Model):
+    member = models.ForeignKey(HKU_member, on_delete=models.CASCADE)
+    venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
+    enter_time = models.DateTimeField()
+    exit_time = models.DateTimeField()
+    def __str__(self):
+        return f'{self.member} {self.venue}'
