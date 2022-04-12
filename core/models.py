417bdca9f7d9728a5d0fe6e8bc698beaf7844c7c
Tszfone <tszfone6@connect.hku.hk>
2022-04-12 16:52:27 +0800

    Update Django Admin table display name

--- core/models.py
+++ core/models.py
@@ -3,8 +3,13 @@ from django.db import models
 class HKU_member(models.Model):
     hku_id = models.CharField(max_length=10, primary_key=True)
     name = models.CharField(max_length=150)
+
     def __str__(self):
         return f'{self.hku_id} {self.name}'
+    
+    class Meta:
+        verbose_name = 'HKU member'
+        verbose_name_plural = 'HKU members'
 
 class Venue(models.Model):
     VENUE_TYPES = (
@@ -16,6 +21,7 @@ class Venue(models.Model):
     location = models.CharField(max_length=150)
     type = models.CharField(max_length=2, choices=VENUE_TYPES)
     capacity = models.IntegerField()
+
     def __str__(self):
         return self.venue_code
 
@@ -24,5 +30,10 @@ class EntryExit(models.Model):
     venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
     enter_time = models.DateTimeField()
     exit_time = models.DateTimeField()
+
     def __str__(self):
         return f'{self.member} {self.venue}'
+
+    class Meta:
+        verbose_name = "EntryExit"
+        verbose_name_plural = 'EntriesExits'
