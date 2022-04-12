d1a72fb81794a52b4c9e22fb90563cdffb92e0c7
zwderek <61866929+zwderek@users.noreply.github.com>
2022-04-12 23:07:42 +0800

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from core import views_api
+from core import views_templates
 
 urlpatterns = [
-    path('contacts', views_api.ContactView.as_view()),
-    path('venues', views_api.VenueView.as_view()),
+    path('contacts', views_templates.ContactView.as_view()),
+    path('venues', views_templates.VenueView.as_view()),
 ]
