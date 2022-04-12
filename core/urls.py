49b8a2c2b067b59ff6d37d22763c22a47666d94f
Yan-jz <62166800+Yan-jz@users.noreply.github.com>
2022-04-13 01:03:46 +0800

    Add files via upload

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,9 @@ from core import views_templates
 urlpatterns = [
     path('contacts', views_templates.ContactView.as_view()),
     path('venues', views_templates.VenueView.as_view()),
+    url(r'^members/$', api_views.Members.as_view()),
+    url(r'^members/(?P<pk>\d+)/$',api_views.Member.as_view()),
+    url(r'^venues/$', api_views.HKU_Venues.as_view()),
+    url(r'^venues/(?P<pk>\d+)/$',api_views.HKU_Venue.as_view()),
+    url(r'^entryexit/$', api_views.Entry_Exits.as_view()),
 ]
