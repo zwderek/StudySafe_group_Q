2665417fd0013745a9b688edf74cf49a2efd1bc3
unknown <zwd1025@connect.hku.hk>
2022-04-12 22:27:22 +0800

    added serializers

--- core/serializers.py
+++ core/serializers.py
@@ -5,13 +5,13 @@ from .models import *
 class HKU_memberSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = Venue
+        model = HKU_member
         fields = '__all__'
 
 class VenueSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = HKU_member
+        model = Venue
         fields = '__all__'
 
 class EntryExitSerializer(serializers.ModelSerializer):
