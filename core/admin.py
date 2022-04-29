d26729d5ad023027355ded13b95205f4e5c880dc
Tszfone <60818208+Tszfone@users.noreply.github.com>
2022-04-30 00:35:17 +0800

    Update core Models List Display in Django Admin

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(HKU_member)
-admin.site.register(Venue)
-admin.site.register(EntryExit)
+class HKU_MemberAdmin(admin.ModelAdmin):
+    list_display = ('hku_id', 'name')
+
+class VenueAdmin(admin.ModelAdmin):
+    list_display = ('venue_code', 'location', 'type', 'capacity')
+
+class EntryExitAdmin(admin.ModelAdmin):
+    list_display = ('member', 'venue', 'enter_time', 'exit_time')
+
+admin.site.register(HKU_member, HKU_MemberAdmin)
+admin.site.register(Venue, VenueAdmin)
+admin.site.register(EntryExit, EntryExitAdmin)
 
